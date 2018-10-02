269f67cbaa021750ecbfcf4e683cd04d88c385c4
Age <ajfleck13@gmail.com>
2018-10-02 13:57:31 -0500

    Add modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,44 @@
-$('#exampleModalCenter').show();
+$('#Modalsubmit').show();
 
 let run = function(){
-    
-const baseURL = "https://api.github.com";
+    const baseURL = "https://api.github.com";
 
-let login = prompt("Please provide your repository URL.");
-
-let loginArray = login.split("/");
-
-let indexG = loginArray.indexOf("github.com");
-
-let username = loginArray[indexG + 1];
-
-let repo = loginArray[indexG + 2];
-
-
-
-$.ajax({
-    url: baseURL + `/repos/${username}/${repo}/issues`,
-    method: "GET", 
-}) .then (function(response){
-    // console.log(response);
-    for(let i = 0; i < response.length; i++){
-        let issues = {
-            title: response[i].title,
-            body: response[i].body,
-            number: response[i].number,
-            login: response[i].user.login,
-            avatar: response[i].user.avatar_url,
-            html: response[i].user.html_url,
-        };
-        issueArray.push(issues);
+    if(!$("#repository").val())
+    {
+        return;
     }
-    rendercard();
-})
 
+    $('#Modalsubmit').hide();
+
+    let repositoryinput = $("#repository").val();
+    let inputArray = repositoryinput.split("/");
+
+    let indexG = inputArray.indexOf("github.com");
+    let username = inputArray[indexG + 1];
+    let repo = inputArray[indexG + 2];
+
+    $.ajax({
+        url: baseURL + `/repos/${username}/${repo}/issues`,
+        method: "GET", 
+    }) .then (function(response){
+        // console.log(response);
+        for(let i = 0; i < response.length; i++){
+            let issues = {
+                title: response[i].title,
+                body: response[i].body,
+                number: response[i].number,
+                login: response[i].user.login,
+                avatar: response[i].user.avatar_url,
+                html: response[i].user.html_url,
+            };
+            issueArray.push(issues);
+        }
+        rendercard();
+    })
 }
+
+let modalsubmit = $('#submit');
+modalsubmit.click(run);
 
 let issueArray = [];
 run();
