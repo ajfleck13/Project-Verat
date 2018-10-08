67c7ba13f043baf67ba0bb1e849be9d42391f290
Rilee Fisher <rilee@fisherpride.comgit config --global rfisher58 Rilee>
2018-10-08 09:46:55 -0500

    none

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,9 +22,10 @@ let run = function() {
         "state": "all"
     })
 
+    urlRepo = baseURL + `/repos/${username}/${repo}/issues` + params,
 
     $.ajax({
-        url: baseURL + `/repos/${username}/${repo}/issues` + params,
+        url: urlRepo,
         method: "GET",
     }).then(function(response) {
         // console.log(response);
@@ -47,11 +48,16 @@ let run = function() {
                 state: response[i].state,
             };
             issueArray.push(issues);
+            
         }
+        $('#title').append(`<a href=${urlRepo}>${repo}</a>`)
         renderDivCards("loader");
     })
 
     // label dropdown
+
+    // 
+
 
     // creating an array of the labels which are assigned to the issues in github. 
 
